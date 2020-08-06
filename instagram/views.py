d2753bb35f61527935202253b8f0c5f7634e4d40
Maureen435 <mchepkemoi435@gmail.com>
2020-08-06 10:48:45 +0300

    added requests to views

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from . models import *
+from django.contrib.auth.forms import UserCreationForm
+from .forms import ImageForm,ProfileForm,CommentsForm
+from django.contrib.auth import login 
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    images = Image.objects.all()
+    comments = Comments.objects.all()
+    return render(request, 'instagram/index.html', {"images":images,"comments":comments})
+    
