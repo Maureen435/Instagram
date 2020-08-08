0bdfe20b2177302f5447361650c6c345daf9178b
Maureen435 <mchepkemoi435@gmail.com>
2020-08-08 09:46:29 +0300

    modified admin site

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Image, Profile, Comments
+admin.site.site_header='Developer'
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comments)
